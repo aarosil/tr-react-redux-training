95912fa008c62d84e6273fbea95d3e2e10033adf
Aaron Siladi <aarons@taskrabbit.com>
2017-04-20 10:59:30 -0700

    search static user db with sync redux action

---
+++ src/redux/modules/syncUserSearch.js
@@ -0,0 +1,37 @@
+import users from './userDb';
+
+const initialState = {
+  results: [],
+  searchKey: 'name'
+}
+
+const SEARCH = 'SEARCH';
+const CHANGE_KEY = 'CHANGE_KEY';
+
+export default function userSync(state = initialState, action) {
+  switch (action.type) {
+
+    case CHANGE_KEY:
+      return {
+        ...state,
+        searchKey: action.key
+      };
+
+    case SEARCH:
+      const results = action.query.length
+        ? users.filter(user => user[state.searchKey].includes(action.query))
+        : []
+
+      return {
+        ...state, results
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const performSearch = query => ({
+  type: SEARCH,
+  query
+});
