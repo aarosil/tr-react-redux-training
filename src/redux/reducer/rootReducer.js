af8b8c6d10d85de93f8fabf73bdac574647b6a9a
Aaron Siladi <aarons@taskrabbit.com>
2017-04-20 10:59:30 -0700

    search static user db with sync redux action

--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -1,10 +1,7 @@
-const initialState = {};
+import { combineReducers } from 'redux';
 
-function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
+import sync from '../modules/syncUserSearch';
 
-export default rootReducer;
+export default combineReducers({
+  sync
+});
